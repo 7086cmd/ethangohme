e0eceb60ff592b3f055b49f0a94604e43e6ac80a
Wu Chengyu <7086cmd@gmail.com>
2024-02-11 22:22:26 +0800

    feat: add my personal elements

--- src/logics/projects.ts
+++ src/logics/projects.ts
@@ -4,27 +4,4 @@ export const projects: Array<{
   description: string
   websiteLink?: string
 }> = [
-  {
-    name: 'Lightest',
-    repoLink: 'zhangzheheng12345/lightest',
-    description: `An extremely light, header-only but meanwhile strong, flexible and
-    customizable C++ unit test library.`
-  },
-  {
-    name: 'birthday-count-downer',
-    repoLink: 'zhangzheheng12345/birthday-count-downer',
-    description: `A simple birthday-counting-down machine.`,
-    websiteLink: 'birthday-count-downer.netlify.app'
-  },
-  {
-    name: 'ZVMS 4',
-    repoLink: 'zvms/zvms4-fronend',
-    description: 'Zhenhai High School Volunteer Management System Version 4.'
-  },
-  {
-    name: 'Wordland 文字岛 ',
-    repoLink: 'zhangzheheng12345/wordland',
-    description: 'A land for words. 🏝️📜 随心而写 随心而阅',
-    websiteLink: 'wordland.site'
-  }
 ]
